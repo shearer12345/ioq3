124f0947c70745f9f98a495e6c065269dd49cce8
shearer12345 <shearer12345@gmail.com>
2015-08-17 15:00:59 +0100

    make STANDALONE

--- code/qcommon/q_shared.h
+++ code/qcommon/q_shared.h
@@ -23,18 +23,20 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #ifndef __Q_SHARED_H
 #define __Q_SHARED_H
 
+#define STANDALONE // building without depending on the Q3 media pak0.pk3
+
 // q_shared.h -- included first by ALL program modules.
 // A user mod should never modify this file
 
 #ifdef STANDALONE
-  #define PRODUCT_NAME			"iofoo3"
-  #define BASEGAME			"foobar"
-  #define CLIENT_WINDOW_TITLE     	"changeme"
-  #define CLIENT_WINDOW_MIN_TITLE 	"changeme2"
-  #define HOMEPATH_NAME_UNIX		".foo"
-  #define HOMEPATH_NAME_WIN		"FooBar"
+  #define PRODUCT_NAME			"ioq3"
+  #define BASEGAME			"baseoa"
+  #define CLIENT_WINDOW_TITLE     	"ioquake3 - openarena"
+  #define CLIENT_WINDOW_MIN_TITLE 	"ioquake3"
+  #define HOMEPATH_NAME_UNIX		".oa"
+  #define HOMEPATH_NAME_WIN		"openArena"
   #define HOMEPATH_NAME_MACOSX		HOMEPATH_NAME_WIN
-  #define GAMENAME_FOR_MASTER		"foobar"	// must NOT contain whitespace
+  #define GAMENAME_FOR_MASTER		"openArena"	// must NOT contain whitespace
 //  #define LEGACY_PROTOCOL	// You probably don't need this for your standalone game
 #else
   #define PRODUCT_NAME			"ioq3"
